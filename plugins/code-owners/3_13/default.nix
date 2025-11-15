3ce9cbdac4ac75d83846e008ecc52c53a5503b6c
Raito Bezarius <masterancpp@gmail.com>
2025-11-15 03:11:21 +0100

    gerrit/3_13: init at 3.13.1
    We drop 3.11.x as part of our maintenance policy.
    
    https://www.gerritcodereview.com/3.13.html
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- plugins/code-owners/3_13/default.nix
+++ plugins/code-owners/3_13/default.nix
@@ -8,9 +8,9 @@ buildGerritBazelPlugin rec {
   version = "${lib.substring 0 7 (src.rev or "dirty")}";
   src = fetchgit {
      url = "https://gerrit.googlesource.com/plugins/code-owners";
-      # stable-3.11 as of 2025-06-22.
-      rev = "323341dd81d8197ee4acd012a6611085009b66b5";
-      hash = "sha256-46TEuQQTifUCJjWzu6D3sxWY83xZ2eLbzT96TRGnjuo=";
+      # master as of 2025-11-15.
+      rev = "6516292dec2924caeed4e7833164a096d921ca31";
+      hash = "sha256-UjF3hSyY6ae+mcceOEKN778a4Tm3PPon/c3GvwlBST8=";
   };
   patches = [
     ./using-usernames.patch
